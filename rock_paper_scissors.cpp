83491487aeddb63b14953b4aaa57886c801ba9a9
Onur Çepni <94144681+onurcepnii@users.noreply.github.com>
2025-01-13 20:07:50 +0300

    Update rock_paper_scissors.cpp

--- rock_paper_scissors.cpp
+++ rock_paper_scissors.cpp
@@ -1,3 +1,21 @@
+/*
+Taş - Kağıt - Makas iki kişi ile oynanan bir oyundur.
+
+Oyuncular aynı anda elini açarak bir sembol gösterir:
+
+Yumruk => Taş
+Açık el => Kağıt
+İşaret ve orta parmağın gösterilmesi => Makas
+Kazanan aşağıdaki kurallara göre belirlenir:
+
+kağıt taşı yener (örter).
+taş makası yener (kırar).
+makas kağıdı yener (keser).
+Bilgisayar ve gerçek bir oyuncu arasında Taş-Makas-Kağıt oynayan bir C++ programı yazınız.
+Oyuncu oyundan çıkmadan önce oyunu n kez oynayabilmelidir.
+
+*/
+
 #include <iostream>
 
 enum class moves {
